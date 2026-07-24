05a7a7f35230c257c0e1912fe39decd63f5ecd43
LuisGustav18 <luis0gustavo43@gmail.com>
2026-07-24 20:12:06 -0300

    Configurações inicias de projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-    <h1>Hello, {{ title() }}</h1>
-
-    <router-outlet />
-  `,
+  templateUrl: './app.html',
   styles: [],
 })
 export class App {
