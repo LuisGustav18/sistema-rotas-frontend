2c16fcd978e675d9ff8c1c9963f97f9132ac272c
LuisGustav18 <luis0gustavo43@gmail.com>
2026-07-27 21:14:40 -0300

    Criando rota de componente HOME

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:"", redirectTo: 'home', pathMatch: 'full' }, // Rediciona quando o caminho estiver apenas vazio para home
+    { path:'home', component: HomeComponent }
+];
