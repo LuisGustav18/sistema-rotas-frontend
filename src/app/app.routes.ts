87e860bc6025e9da7a7f5ef80c02a5cd062fc225
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-12 11:34:41 -0300

    Rotas para LOGIN feitas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path:"", redirectTo: 'home', pathMatch: 'full' }, // Rediciona quando o caminho estiver apenas vazio para home
     { path:"", component: HeaderComponent, children: [
         { path:'home', component: HomeComponent }
     ]
-    }
+    },
+    { path: "login", component: LoginComponent}
 ];
