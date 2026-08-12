ed38b422f674eaf038ad76efecbc422a4e17cea6
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-12 17:12:37 -0300

    HTML e CSS de LOGIN feito

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIcon
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
