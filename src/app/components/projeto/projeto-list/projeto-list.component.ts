1c6027e52581f6fc725ef1435666c8316fefd244
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-18 14:15:18 -0300

    Criado componente PROJETO-LIST

---
+++ src/app/components/projeto/projeto-list/projeto-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projeto-list',
+  imports: [],
+  templateUrl: './projeto-list.component.html',
+  styleUrl: './projeto-list.component.scss',
+})
+export class ProjetoListComponent {}
