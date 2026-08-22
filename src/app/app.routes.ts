18772e07e2b012395aced5dd428ab1a739033bff
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-21 23:00:37 -0300

    HTML e CSS de componente PROJETO-LIST feito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,20 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { ProjetoListComponent } from './components/projeto/projeto-list/projeto-list.component';
+import { AppLayoutComponent } from './components/app-layout/app-layout.component';
 
 export const routes: Routes = [
-    { path:"", redirectTo: 'home', pathMatch: 'full' }, // Rediciona quando o caminho estiver apenas vazio para home
-    { path:"", component: HeaderComponent, children: [
-        { path:'home', component: HomeComponent }
-    ]
+    { path: "", redirectTo: 'home', pathMatch: 'full' }, // Rediciona quando o caminho estiver apenas vazio para home
+    {
+        path: "", component: HeaderComponent, children: [
+            { path: 'home', component: HomeComponent },
+        ]
     },
-    { path: "login", component: LoginComponent},
-    { path: "cadastro", component: CadastroComponent}
+    { path: "login", component: LoginComponent },
+    { path: "cadastro", component: CadastroComponent },
+
+    { path: "", component: AppLayoutComponent, children: [
+        { path:'projeto', component:ProjetoListComponent },
+    ]}
 ];
--- src/app/components/projeto/projeto-list/projeto-list.component.ts
+++ src/app/components/projeto/projeto-list/projeto-list.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-projeto-list',
-  imports: [],
+  imports: [
+      CommonModule,
+      MatButtonModule
+  ],
   templateUrl: './projeto-list.component.html',
   styleUrl: './projeto-list.component.scss',
 })
-export class ProjetoListComponent {}
+export class ProjetoListComponent {
+
+  dado: boolean = false;
+
+}
