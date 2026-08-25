23aef649d225f28ba34b330f3fcd99a748f555df
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-25 12:30:03 -0300

    Criado componente ROTA-CREATE

---
+++ src/app/components/rota/rota-create/rota-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rota-create',
+  imports: [],
+  templateUrl: './rota-create.component.html',
+  styleUrl: './rota-create.component.scss',
+})
+export class RotaCreateComponent {}
