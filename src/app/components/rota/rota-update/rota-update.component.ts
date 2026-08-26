e7edf7afa04b9480097cbf7f4df70b64c7ac6e03
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-26 15:25:23 -0300

    Criado componente ROTA-UPDATE

---
+++ src/app/components/rota/rota-update/rota-update.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rota-update',
+  imports: [],
+  templateUrl: './rota-update.component.html',
+  styleUrl: './rota-update.component.scss',
+})
+export class RotaUpdateComponent {}
