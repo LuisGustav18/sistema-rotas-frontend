48b886579280a7e1904447b4a62a81a275f450a2
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-27 19:21:31 -0300

    Criado componente PROJETO-UPDATE

---
+++ src/app/components/projeto/projeto-update/projeto-update.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projeto-update',
+  imports: [],
+  templateUrl: './projeto-update.component.html',
+  styleUrl: './projeto-update.component.scss',
+})
+export class ProjetoUpdateComponent {}
