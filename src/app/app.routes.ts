21f16d49cd75030cc975f494f94b923b494417c1
LuisGustav18 <luis0gustavo43@gmail.com>
2026-08-27 19:19:15 -0300

    HTML e CSS de ROTA-UPDATE feito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { AppLayoutComponent } from './components/app-layout/app-layout.component
 import { RotaListComponent } from './components/rota/rota-list/rota-list.component';
 import { RotaCreateComponent } from './components/rota/rota-create/rota-create.component';
 import { ProjetoCreateComponent } from './components/projeto/projeto-create/projeto-create.component';
+import { RotaUpdateComponent } from './components/rota/rota-update/rota-update.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: 'home', pathMatch: 'full' }, // Rediciona quando o caminho estiver apenas vazio para home
--- src/app/components/rota/rota-update/rota-update.component.ts
+++ src/app/components/rota/rota-update/rota-update.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-rota-update',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterModule,
+    MatIcon
+  ],
   templateUrl: './rota-update.component.html',
   styleUrl: './rota-update.component.scss',
 })
-export class RotaUpdateComponent {}
+export class RotaUpdateComponent {
+
+  onFileSelected(event: Event){
+  }
+}
