bd27e7c923494d0438e2fd4602db053a42c605b0
LuisGustav18 <luis0gustavo43@gmail.com>
2026-09-13 18:25:59 -0300

    Criado componente PROJETO-DELETE

---
+++ src/app/components/projeto/projeto-delete/projeto-delete.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projeto-delete',
+  imports: [],
+  templateUrl: './projeto-delete.component.html',
+  styleUrl: './projeto-delete.component.scss',
+})
+export class ProjetoDeleteComponent {}
