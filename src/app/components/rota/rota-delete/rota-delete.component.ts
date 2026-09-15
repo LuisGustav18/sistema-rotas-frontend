c53b7ea8ec532aecf7ff615c238c342d54790a0d
LuisGustav18 <luis0gustavo43@gmail.com>
2026-09-15 15:12:09 -0300

    Criado componente ROTA-DELETE

---
+++ src/app/components/rota/rota-delete/rota-delete.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rota-delete',
+  imports: [],
+  templateUrl: './rota-delete.component.html',
+  styleUrl: './rota-delete.component.scss',
+})
+export class RotaDeleteComponent {}
